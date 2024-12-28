4bcc1ec0e9f229d41352851c895634fe37450ef0
Bryan Lee <bryan.lee.jh@gmail.com>
2024-12-28 17:35:36 +0700

    feat: add styles for results

--- src/components/results.tsx
+++ src/components/results.tsx
@@ -24,19 +24,19 @@ const escapeRegExp = (string: string) => {
 };
 
 const Results = ({ searchResults, totalResults, query }: ResultsProps) => (
-  <div className="mt-8 mx-[160px] pr-[160px]">
+  <div className="mt-8 mx-[160px]">
     <div className="font-semibold text-[22px] leading-7 font-['Open_Sans'] mb-10">
       Showing 1-10 of {totalResults} results
     </div>
-    <div className="space-y-8">
+    <div className="space-y-8 w-[816px]">
       {searchResults.map((result) => (
         <div key={result.title + result.date} className="space-y-2">
-          <h2 className="text-xl">
+          <h2 className="font-semibold text-[22px] leading-7 font-sans">
             <a href={result.url} className="text-blue-600 hover:underline">
               {highlightText(result.title, query)}
             </a>
           </h2>
-          <p className="text-sm text-gray-600">
+          <p className="font-sans text-base font-normal leading-6 text-left">
             {result.date} — {highlightText(result.description, query)}
           </p>
           <p className="text-sm text-gray-500">{result.url}</p>
